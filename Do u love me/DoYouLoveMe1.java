37f5196b236e07e766637eaeed20a03ecc41c948
Ayush-may <112775332+Ayush-may@users.noreply.github.com>
2023-01-07 22:11:03 +0530

    Add files via upload
    Noo button Changes it position everytime mouse points on it

---
+++ Do u love me/DoYouLoveMe1.java
@@ -0,0 +1,15 @@
+package fun;
+
+import javax.swing.JFrame;
+
+public class DoYouLoveMe1 {
+	JFrame frame=new JFrame("Wanna Hang out...??");
+	DoYouLoveMe1(){
+		
+		frame.setVisible(true);
+	}
+	public static void main(String[] args) {
+		new DoYouLoveMe1();
+	}
+
+}
